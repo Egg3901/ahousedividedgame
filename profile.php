990bd47aa7b7c4c7d2db0afb458f06de0dc8b23a
t. who <masonlovell2013@gmail.com>
2021-01-20 17:24:04 -0500

    changes to connection system

--- profile.php
+++ profile.php
@@ -5,14 +5,7 @@ if (!isset($_SESSION['loggedin'])) {
     header('Location: index.html');
     exit;
 }
-$DATABASE_HOST = 'mi3-ss55';
-$DATABASE_USER = 'ahousedi_ahousedi';
-$DATABASE_PASS = '3g6V27LEmDCUFnv';
-$DATABASE_NAME = 'ahousedi_phplogin';
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
+include 'connect.php';
 
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
 $stmt = $con->prepare('SELECT password, email, influence, polstate, polname, imgurl, social, economic FROM accounts WHERE id = ?');
