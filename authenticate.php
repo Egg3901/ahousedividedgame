88f4c74a80c57d450b02de9ba347b0ec12a4f787
t. who <masonlovell2013@gmail.com>
2021-01-20 16:21:24 -0500

    typo fixed

--- authenticate.php
+++ authenticate.php
@@ -3,7 +3,7 @@ session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'ahousedi_phplogin';
-$DATABASE_PASS = '3g6V27LEmDCUFnv'
+$DATABASE_PASS = '3g6V27LEmDCUFnv';
 $DATABASE_NAME = 'ahousedi_phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS,$DATABASE_NAME);
