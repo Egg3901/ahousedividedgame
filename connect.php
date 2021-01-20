95d1dd60259c03dd22b3f85be416cb4268c00f74
t. who <masonlovell2013@gmail.com>
2021-01-20 17:22:06 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php// Change this to your connection info.
-$DATABASE_URL = 'http://ahousedivided.a2hosted.com/';
+$DATABASE_HOST = 'ahdgame';
 $DATABASE_USER = 'ahousedi_ahousedi';
 $DATABASE_PASSWORD = '3g6V27LEmDCUFnv';
 $DATABASE_NAME = 'ahousedi_phplogin';
 // Try and connect using the info above.
-$con = mysqli_connect($DATABASE_URL, $DATABASE_USER, $DATABASE_PASSWORD, $DATABASE_NAME);
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASSWORD, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     // If there is an error with the connection, stop the script and display the error.
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
