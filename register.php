765935cc9f243659e4ead26d030f8446fab4d21e
t. who <masonlovell2013@gmail.com>
2021-01-20 16:06:22 -0500

    updated/deployed registration information accurate to server

--- register.php
+++ register.php
@@ -1,9 +1,8 @@
 <?php
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '.66fHTmBZRK6sAu';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_USER = 'ahousedi@localhost';
+$DATABASE_NAME = 'ahousedi_phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
