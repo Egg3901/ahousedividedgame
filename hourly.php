35dee5af894881708796592adf9ea44e22f41dce
t. who <masonlovell2013@gmail.com>
2021-01-20 21:23:37 -0500

    hourly update script test

--- hourly.php
+++ hourly.php
@@ -1,4 +1,7 @@
 <?php
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 $con = OpenCon();
 $uquery = 'UPDATE accounts SET influence = influence * .97';
 $stmt = $con->prepare($uquery);
