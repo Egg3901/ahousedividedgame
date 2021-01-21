9bc8e9f03e08bf8bbcb3173e07538da8ee3a5a17
t. who <masonlovell2013@gmail.com>
2021-01-20 20:22:52 -0500

    trying new connect method

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $DATABASE_USER = 'AHDAdmin';
 $DATABASE_PASSWORD = '3ED*3^^e70WZl';
 $DATABASE_NAME = 'ahousedi_phplogin';
 // Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASSWORD, $DATABASE_NAME);
+$con = new PDO('mysql:host=localhost;dbname=ahousedi_phplogin', 'ahousedi_ahousedi', '26Uz7wV4:]rYuN');
 if (mysqli_connect_errno()) {
     // If there is an error with the connection, stop the script and display the error.
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
